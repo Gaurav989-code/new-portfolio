635f9f1f3e69908ea451a1786b6f500dff82a61b
gauravtekude <gauravtekude1998@gmail.com>
2025-05-21 13:23:49 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { ThemeToggle } from '../components/TheameToggle'
+import StarBackGround from '../components/StarBackGround'
+import Navbar from '../components/Navbar'
+import HeroSection from '../components/HeroSection'
+import AboutMe from '../components/AboutMe'
+import SkillsSection from '../components/SkillsSection'
+import ProjectsSection from '../components/ProjectsSection'
+import ContactSection from '../components/ContactSection'
+import Footer from '../components/Footer'
+
+
+const Home = () => {
+  return (
+    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+      <ThemeToggle />
+      <StarBackGround />
+      <Navbar />
+
+      <main>
+        <HeroSection />
+        <AboutMe />
+        <SkillsSection />
+        <ProjectsSection />
+        <ContactSection />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
