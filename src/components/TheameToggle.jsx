c57f59c7721eb599a8ad5750862849c1517e0bac
gauravtekude <gauravtekude1998@gmail.com>
2025-05-21 21:46:27 +0530

    cn change

--- src/components/TheameToggle.jsx
+++ src/components/TheameToggle.jsx
@@ -1,6 +1,5 @@
 import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react";
-import { cn } from "../lib/utils";
 
 export const ThemeToggle = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -30,12 +29,10 @@ export const ThemeToggle = () => {
   };
 
   return (
-     <button
+    <button
       onClick={toggleTheme}
-      className={cn(
-        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors  duration-300",
-        "focus:outline-hidden cursor-pointer"
-      )}
+      className="fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors  duration-300
+        focus:outline-hidden cursor-pointer"
     >
       {isDarkMode ? (
         <Sun className="h-6 w-6 text-yellow-300" />
